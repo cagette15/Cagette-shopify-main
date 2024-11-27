2b6cdb17543c41b8b4e6e2b67b68ee2bfb739f6e
Abdul Basit <104556643+proabdulbasit@users.noreply.github.com>
2024-11-27 10:56:00 +0500

    Change the hidden-homepage-carousel to christmas

--- components/eshop/carousel-eshop.tsx
+++ components/eshop/carousel-eshop.tsx
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export async function EshopCarousel() {
   // Collections that start with `hidden-*` are hidden from the search page.
-  const products = await getCollectionProducts({ collection: 'hidden-homepage-carousel' });
+  const products = await getCollectionProducts({ collection: 'christmas' });
 
   if (!products?.length) return null;
 
