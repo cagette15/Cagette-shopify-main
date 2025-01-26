c0a5b30f019579c086c293bc9967f674f4c795a3
Abdul Basit <104556643+proabdulbasit@users.noreply.github.com>
2025-01-26 13:12:35 +0500

    Update slider

--- components/eshop/sliderone.tsx
+++ components/eshop/sliderone.tsx
@@ -28,13 +28,13 @@ export default function EshopSliderOne() {
             <div className="justify-left flex h-full w-full items-center bg-black bg-opacity-50">
               <div className="container  text-center text-white">
                 <h3 className="text-3xl font-bold text-white drop-shadow-[0_35px_35px_rgba(0,0,0,0.65)] md:text-5xl">
-                  CHRISTMAS DELICACIES
+                  Our Gourmet Selection
                 </h3>
                 <h2 className="text-3xl font-bold text-white drop-shadow-[0_35px_35px_rgba(0,0,0,0.65)] md:text-5xl">
-                   Available now!
+                   FRENCH CHEESE
                 </h2>
                 <h6 className="py-2 text-lg text-white drop-shadow-[35px_35px_35px_rgba(0,0,0,0.65)] md:text-2xl">
-                  Discover our offers for the festivities... <br /> <br />
+Over 40 cheeses imported weekly from France<br /> <br />
                  <Link href="search/christmas">
                     <span className="w-fit rounded-full bg-second px-4 py-2 text-lg font-bold text-white drop-shadow-[35px_35px_35px_rgba(0,0,0,0.65)] md:text-lg">
                       Shop Here
